927426cf7164cd21ad7640910d536bfef106662e
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-13 00:39:03 +0100

    Date formatting

--- src/main/java/fx/mastercalss/todolist/Controller.java
+++ src/main/java/fx/mastercalss/todolist/Controller.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,7 +52,8 @@ public class Controller {
                 if(newValue!=null){
                     TodoItem item = todoListView.getSelectionModel().getSelectedItem();
                     itemDetailsTextArea.setText(item.getDetails());
-                    deadLineLabel.setText(item.getDeadline().toString());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadLineLabel.setText(df.format(item.getDeadline()));
                 }
             }
         });
