28144e3853bbb7fa2bd16da77935b025d5628ed9
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-17 01:01:32 +0100

    key events and toolbar

--- src/main/java/fx/mastercalss/todolist/Controller.java
+++ src/main/java/fx/mastercalss/todolist/Controller.java
@@ -9,6 +9,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
@@ -124,6 +126,17 @@ public class Controller {
             System.out.println("OK pressed");
         }
     }
+
+    @FXML
+    public void handleKeyPressed(KeyEvent keyEvent){
+        TodoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+        if(selectedItem!=null){
+            if(keyEvent.getCode().equals(KeyCode.DELETE)){
+                deleteItem(selectedItem);
+            }
+        }
+    }
+
 
     @FXML
     public void handleClickListView(){
