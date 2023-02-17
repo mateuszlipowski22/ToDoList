4d078c116a779de85cd044d4e9a4650330c4e00d
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-17 01:21:36 +0100

    sorted list

--- src/main/java/fx/mastercalss/todolist/Controller.java
+++ src/main/java/fx/mastercalss/todolist/Controller.java
@@ -4,6 +4,7 @@ import fx.mastercalss.todolist.datamodel.TodoData;
 import fx.mastercalss.todolist.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -18,6 +19,7 @@ import javafx.util.Callback;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,6 +40,9 @@ public class Controller {
 
     @FXML
     private ContextMenu listContextMenu;
+
+    @FXML
+    private ToggleButton filterToggleButton;
 
     public void initialize(){
         listContextMenu = new ContextMenu();
@@ -64,7 +69,15 @@ public class Controller {
             }
         });
 
-        todoListView.setItems(TodoData.getInstance().getTodoItems());
+        SortedList<TodoItem> sortedList = new SortedList<TodoItem>(TodoData.getInstance().getTodoItems(), new Comparator<TodoItem>() {
+            @Override
+            public int compare(TodoItem o1, TodoItem o2) {
+                return o1.getDeadline().compareTo(o2.getDeadline());
+            }
+        });
+
+//        todoListView.setItems(TodoData.getInstance().getTodoItems());
+        todoListView.setItems(sortedList);
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
 
@@ -154,6 +167,14 @@ public class Controller {
 
         if(result.isPresent() && result.get()==ButtonType.OK){
             TodoData.getInstance().deleteTodoItem(item);
+        }
+    }
+
+    public void handleFilterButton(){
+        if(filterToggleButton.isSelected()){
+
+        }else {
+
         }
     }
 
